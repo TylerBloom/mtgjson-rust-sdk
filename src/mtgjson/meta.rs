78357836cc232669b9e942f75e8be58f5c26fe9b
TylerBloom <tylerbloom2222@gmail.com>
2022-06-11 13:09:23 -0400

    Updated meta struct

--- src/mtgjson/meta.rs
+++ src/mtgjson/meta.rs
@@ -3,8 +3,8 @@ use serde_with::skip_serializing_none;
 
 #[allow(non_snake_case)]
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct Meta {
-    date: String,
-    version: String,
+    pub date: String,
+    pub version: String,
 }
